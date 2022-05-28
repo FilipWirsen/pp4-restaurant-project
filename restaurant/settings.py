df6a96918f1d81de5e566dada984c8ec5f4cd9fd
FilipWirsen <Filip.wirsen99@gmail.com>
2022-05-28 12:59:24 +0000

    Add accounts app and create create/login urls

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHasedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
