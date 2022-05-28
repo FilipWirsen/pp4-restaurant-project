f2fb80de13f6ba05be5b102037e512755422cff0
FilipWirsen <Filip.wirsen99@gmail.com>
2022-05-28 15:32:15 +0000

    test for heroku

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -145,6 +145,9 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/static/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
