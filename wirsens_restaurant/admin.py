6be64f13a812d2a54f9483d88227cd2eab211496
FilipWirsen <Filip.wirsen99@gmail.com>
2022-06-13 15:39:54 +0000

    Change search field in admin panel

--- wirsens_restaurant/admin.py
+++ wirsens_restaurant/admin.py
@@ -7,7 +7,7 @@ class ReservationAdmin(admin.ModelAdmin):
     list_filter = ('book_date', 'book_time')
     list_display = (
         'user', 'table', 'book_date', 'book_time', 'reservation_id')
-    search_fields = ['user']
+    search_fields = ['user__username']
 
 
 admin.site.register(Table)
