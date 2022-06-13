1992ffdf49e647a2ae64e45dbf826cf54e2b4110
FilipWirsen <Filip.wirsen99@gmail.com>
2022-06-13 15:09:00 +0000

    Remove bookings view for superusers since they can see everything in admin panel

--- wirsens_restaurant/urls.py
+++ wirsens_restaurant/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('manage-reservation', views.ReservationDetail.as_view(), name='manage-reservation'),
     path('update-reservation/<int:reservation_id>', views.update_reservation, name='update-reservation'),
     path('delete-reservation/<int:reservation_id>', views.delete_reservation, name='delete-reservation'),
-    path('bookings', views.view_bookings, name='bookings')
 ]
